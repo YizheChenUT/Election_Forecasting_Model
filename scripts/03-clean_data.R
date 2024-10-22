833b6fcac1a40dec5d32a802b61ba240cf89c80e
YizheChenUT <yz.chen@mail.utoronto.ca>
2024-10-22 20:01:21 +0000

    Update clean method.

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -18,16 +18,20 @@ raw_data <- read_csv("data/01-raw_data/raw_data.csv")
 cleaned_data <-
   raw_data |>
   janitor::clean_names() |>  # Clean column names to snake_case
-  select(poll_id, pollster, pollster_rating_name, pollster_rating_id, candidate_name, pct, party, start_date, end_date) |> 
+  filter(pollster == "YouGov") |>  # Pick one pollster
+  select(poll_id, pollster, candidate_name, pct, party, start_date, end_date, sample_size) |> 
   filter(!is.na(pct)) |>      # Filter out rows where percentage (pct) is missing
   mutate(
     party = case_when(
       party == "DEM" ~ "Democratic",
       party == "REP" ~ "Republican",
       TRUE ~ party    # Keep other parties unchanged
-    )
+    ),
+    candidate_name = as.factor(candidate_name),
+    start_date = as.Date(start_date),  # Assuming start_date is in standard format
+    end_date = mdy(end_date)  # If end_date is in "mm/dd/yyyy" format, adjust based on your data
   ) |> 
-  arrange(start_date)           # Arrange data by poll date
+  arrange(start_date)          # Arrange data by the 'start_date'
 
 #### Save data ####
 write_csv(cleaned_data, "data/02-analysis_data/analysis_data.csv")
