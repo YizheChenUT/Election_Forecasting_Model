ac688cf012b1a6a59e2b0d35b9b9595a1ab196ce
YizheChenUT <yz.chen@mail.utoronto.ca>
2024-10-22 18:07:48 +0000

    Cleans the raw presidential polling data from FiveThirtyEight.

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,35 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw presidential polling data from FiveThirtyEight.
+# Author: Yizhe Chen
+# Date: 22 OCT 2024
+# Contact: yz.chen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Must have downloaded the raw data.
+# Any other information needed? No
 
 #### Workspace setup ####
 library(tidyverse)
+library(janitor)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/01-raw_data/raw_data.csv")
 
+# Select relevant columns and clean names
 cleaned_data <-
   raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
+  janitor::clean_names() |>  # Clean column names to snake_case
+  select(poll_id, pollster, pollster_rating_name, pollster_rating_id, candidate_name, pct, party, start_date, end_date) |> 
+  filter(!is.na(pct)) |>      # Filter out rows where percentage (pct) is missing
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+    party = case_when(
+      party == "DEM" ~ "Democratic",
+      party == "REP" ~ "Republican",
+      TRUE ~ party    # Keep other parties unchanged
+    )
+  ) |> 
+  arrange(start_date)           # Arrange data by poll date
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "data/02-analysis_data/analysis_data.csv")
+
+print("Data cleaned and saved successfully.")
