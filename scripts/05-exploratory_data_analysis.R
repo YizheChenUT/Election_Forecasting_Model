997f9ff15d213406762671e5fc2060ba8c831c6d
YizheChenUT <yz.chen@mail.utoronto.ca>
2024-10-22 19:36:52 +0000

    Models polling data for YouGov.

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models polling data for YouGov
+# Author: Yizhe Chen
+# Date: 22 OCT 2024
+# Contact: yz.chen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Must have cleaned polling data.
+# Any other information needed? No
 
 
 #### Workspace setup ####
@@ -13,13 +13,18 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_csv("data/02-analysis_data/analysis_data.csv")
+
+#### Filter YouGov data ####
+yougov_data <- analysis_data %>%
+  filter(pollster == "YouGov")
 
 ### Model data ####
-first_model <-
+# Predict candidate percentage ('pct') based on pollster rating and party
+yougov_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
+    formula = pct ~ party,
+    data = yougov_data,
     family = gaussian(),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
@@ -30,8 +35,9 @@ first_model <-
 
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  yougov_model,
+  file = "models/yougov_model.rds"
 )
 
-
+#### Model summary ####
+print(summary(yougov_model))
